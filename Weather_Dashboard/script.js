b62d41003c8a7d391d94bc4b8dee511bb7ada135
Ibnu Afdel <ibnuafdel@gmail.com>
2024-12-28 19:32:38 +0300

    Add error message for empty input

--- Weather_Dashboard/script.js
+++ Weather_Dashboard/script.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cityInput = document.getElementById("city-input");
   const searchBtn = document.getElementById("search-btn");
+  const errorMessage = document.getElementById("error-message");
 
   searchBtn.addEventListener("click", () => {
     const cityName = cityInput.value.trim();
+
+    if (!cityName) {
+      errorMessage.textContent = "Please enter a city name.";
+      errorMessage.classList.remove("hidden");
+      return;
+    }
+
+    errorMessage.classList.add("hidden"); // not working
+
     console.log(`City Name : ${cityName}`);
   });
 });
